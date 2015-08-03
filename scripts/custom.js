24bed68874972b339ed01b47223e7296529a624e
Vijay <vj.scjp@gmail.com>
2015-08-04 01:25:18 +0530

    Changes made
    Done

--- scripts/custom.js
+++ scripts/custom.js
@@ -49,7 +49,12 @@ if(!(navigator.userAgent.match(/iPhone|iPad|iPod|Android|BlackBerry|IEMobile/i))
 			$(".navbar-slide .navbar-toggle").addClass("collapsed");
         }
     });
-	
+
+    $('#slider1') .cycle({
+        fx: 'fade', //'scrollLeft,scrollDown,scrollRight,scrollUp',blindX, blindY, blindZ, cover, curtainX, curtainY, fade, fadeZoom, growX, growY, none, scrollUp,scrollDown,scrollLeft,scrollRight,scrollHorz,scrollVert,shuffle,slideX,slideY,toss,turnUp,turnDown,turnLeft,turnRight,uncover,ipe ,zoom
+        speed:  'slow',
+        timeout: 2000
+    });
 //------------------------------------------------------------------------
 //						NAVBAR HIDE ON CLICK (COLLAPSED) SCRIPT
 //------------------------------------------------------------------------ 		
